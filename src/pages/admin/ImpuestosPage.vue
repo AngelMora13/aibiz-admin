6f833e59d55959cf7764da7a4c373dd56957d230
Ricardo Sequeira <ricardosequeira4@gmail.com>
2024-06-11 15:02:46 -0400

    fix

--- src/pages/admin/ImpuestosPage.vue
+++ src/pages/admin/ImpuestosPage.vue
@@ -73,6 +73,78 @@
           </template>
         </q-table>
       </div>
+      <div class="row justify-center" style="max-width: 100%">
+        <q-table
+          class="col-12"
+          :columns="headersRetIva"
+          :rows="retIvaList"
+          flat
+          no-data-label="no hay datos disponibles"
+          loading-label="Buscando..."
+          rows-per-page-label="filas por pagina"
+          key="_id"
+          row-key="_id"
+          v-model:pagination="paginationRetIva"
+          @request="handleTableUpdate"
+          :loading="loaderRetIva"
+        >
+          <template v-slot:top>
+            <div class="row" style="width: 100%">
+              <h2 class="col-4 texto-3">Retenciones de I.V.A</h2>
+              <div class="col-8 flex justify-end" style="align-items: center">
+                <q-btn
+                  color="black"
+                  class="text-white text-capitalize"
+                  @click="openFormRetIva = true"
+                >
+                  <q-icon name="add" class="q-mr-sm"></q-icon>
+                  Agregar Retención I.V.A
+                </q-btn>
+              </div>
+            </div>
+            <div class="row" style="width: 100%">
+              <q-select
+                v-model="paisRetencionIva"
+                label="País"
+                title="País"
+                :options="listContryOptions"
+                option-label="name"
+                option-value="name"
+                emit-value
+                style="min-width: 180px"
+                dense
+                clearable
+                color="black"
+                use-input
+                input-debounce="0"
+                @filter="filterFn"
+              ></q-select>
+            </div>
+          </template>
+          <template v-slot:body-cell-acciones="{ row }">
+            <q-td style="text-align: center">
+              <q-btn icon="edit" unelevated @click="openEditFormRetIva(row)">
+                <q-tooltip
+                  anchor="top middle"
+                  self="bottom middle"
+                  :offset="[10, 10]"
+                >
+                  Editar
+                </q-tooltip>
+              </q-btn>
+              <q-btn icon="delete" unelevated @click="openDeleteRetIVa(row)">
+                <q-tooltip
+                  anchor="top middle"
+                  self="bottom middle"
+                  :offset="[10, 10]"
+                >
+                  Eliminar
+                </q-tooltip>
+              </q-btn>
+            </q-td>
+          </template>
+        </q-table>
+      </div>
       <div class="row justify-center q-py-md" style="max-width: 100%">
         <q-table
           class="col-12"
@@ -208,6 +280,44 @@
           </div>
         </q-card>
       </q-dialog>
+      <q-dialog v-model="openFormRetIva" @keydown.esc="openFormRetIva = false">
+        <q-card flat class="w-100" style="max-width: 400px">
+          <div class="row justify-end">
+            <q-btn
+              unelevated
+              icon="close"
+              @click="openFormRetIva = false"
+            ></q-btn>
+          </div>
+          <formRetIva
+            :retIvaData="retIvaData"
+            :listContry="listContry"
+            @guardar-retIva="saveRetIva"
+          ></formRetIva>
+        </q-card>
+      </q-dialog>
+      <q-dialog
+        v-model="openDeleteRetIva"
+        @keydown.esc="openDeleteRetIva = false"
+      >
+        <q-card flat class="w-100">
+          <div class="flex justify-center q-pa-md" style="font-size: 14px">
+            <span
+              >¿Está seguro (a) que desea eliminar esta retención de
+              I.V.A?</span
+            >
+          </div>
+          <div class="flex justify-center q-pb-md">
+            <q-btn
+              color="black"
+              class="text-white text-capitalize"
+              @click="deleteRetIva"
+              :loading="loaderDelete"
+              >Aceptar</q-btn
+            >
+          </div>
+        </q-card>
+      </q-dialog>
     </div>
   </q-page>
 </template>
@@ -219,6 +329,7 @@ import XLSX from "xlsx";
 import endpoint from "../../services/Endpoint";
 import { useUserStore } from "stores/user-store";
 import FormIva from "src/components/IvaForm.vue";
+import formRetIva from "src/components/formRetIva.vue";
 import FormIslr from "src/components/FormIslr.vue";
 import Endpoint from "../../services/Endpoint";
 import { debounce } from "quasar";
@@ -226,16 +337,21 @@ import { debounce } from "quasar";
 const userStore = useUserStore();
 const ivaList = ref([]);
 const islrList = ref([]);
+const retIvaList = ref([]);
 const listContry = ref([]);
 const listContryOptions = ref([]);
 const ivaData = ref(null);
 const islrData = ref(null);
+const retIvaData = ref(null);
 const openFormIva = ref(false);
 const openFormIslr = ref(false);
 const openDeleteIva = ref(false);
 const openDialogDeleteIslr = ref(false);
+const openFormRetIva = ref(false);
+const openDeleteRetIva = ref(false);
 const loaderIva = ref(false);
 const loaderIslr = ref(false);
+const loaderRetIva = ref(false);
 const loaderDelete = ref(false);
 const pagination = ref({
   page: 1,
@@ -247,8 +363,14 @@ const paginationIslr = ref({
   rowsPerPage: 5,
   rowsNumber: 0,
 });
+const paginationRetIva = ref({
+  page: 1,
+  rowsPerPage: 5,
+  rowsNumber: 0,
+});
 const pais = ref(null);
 const paisIslr = ref(null);
+const paisRetencionIva = ref(null);
 const headersIva = computed(() => {
   const header = [
     {
@@ -256,6 +378,47 @@ const headersIva = computed(() => {
       align: "center",
       label: "Descripción",
       field: "descripcion",
+      sortable: false,
+    },
+    {
+      name: "iva",
+      align: "center",
+      label: "I.V.A (%)",
+      field: "iva",
+      sortable: false,
+    },
+    {
+      name: "pais",
+      align: "center",
+      label: "País",
+      field: "pais",
+      sortable: false,
+      headerStyle: "white-space: normal",
+    },
+    {
+      name: "acciones",
+      align: "center",
+      label: "Acciones",
+      field: "acciones",
+      sortable: false,
+    },
+  ];
+  return header;
+});
+const headersRetIva = computed(() => {
+  const header = [
+    {
+      name: "descipcion",
+      align: "center",
+      label: "Descripción",
+      field: "descripcion",
+      sortable: false,
+    },
+    {
+      name: "retencion",
+      align: "center",
+      label: "Retencion (%)",
+      field: "retIva",
       sortable: false,
     },
     {
@@ -341,6 +504,7 @@ onMounted(async () => {
   getListContry();
   getListIva();
   getListIslr();
+  getListRetIva();
 });
 const getListIva = async () => {
   try {
@@ -515,6 +679,83 @@ const deleteIslr = async () => {
     loaderDelete.value = false;
   }
 };
+const getListRetIva = async () => {
+  try {
+    loaderRetIva.value = true;
+    const body = {
+      pais: paisRetencionIva.value,
+      itemsPorPagina: paginationRetIva.value.rowsPerPage,
+      pagina: paginationRetIva.value.page,
+    };
+    const { data } = await Endpoint.impuestos({
+      body,
+      path: "get/retIva",
+    });
+    retIvaList.value = data.retIva;
+    paginationRetIva.value.rowsNumber = data.count || 0;
+  } catch (e) {
+    console.log(e);
+    alert(e.response?.data?.error || "Ha ocurrido un error inesperado");
+  } finally {
+    loaderRetIva.value = false;
+  }
+};
+const openEditFormRetIva = (item) => {
+  retIvaData.value = item;
+  openFormRetIva.value = true;
+};
+const saveRetIva = async ($event) => {
+  console.log($event);
+  try {
+    const body = {
+      ...$event,
+    };
+    const { data } = await Endpoint.impuestos({
+      body,
+      path: "save/retIva",
+    });
+    const index = retIvaList.value.findIndex((e) => e._id === $event._id);
+    if (index !== -1) {
+      retIvaList.value.splice(index, 1, data.retIva);
+    } else {
+      retIvaList.value.push(data.retIva);
+    }
+  } catch (e) {
+    console.log(e);
+    alert(e.response?.data?.error || "Ha ocurrido un error inesperado");
+  } finally {
+    openFormRetIva.value = false;
+  }
+};
+const openDeleteRetIVa = (item) => {
+  retIvaData.value = item;
+  openDeleteRetIva.value = true;
+};
+const deleteRetIva = async () => {
+  loaderDelete.value = true;
+  try {
+    const body = {
+      _id: retIvaData.value._id,
+    };
+    const { data } = await Endpoint.impuestos({
+      body,
+      path: "delete/retIva",
+    });
+    const index1 = retIvaList.value.findIndex(
+      (e) => e._id === retIvaData.value._id
+    );
+    if (index1 !== -1) {
+      retIvaList.value.splice(index1, 1);
+    }
+    alert(data.status);
+  } catch (e) {
+    console.log(e);
+    alert(e.response?.data?.error || "Ha ocurrido un error inesperado");
+  } finally {
+    openDeleteIva.value = false;
+    loaderDelete.value = false;
+  }
+};
 const filterFn = (val, update) => {
   if (val === "") {
     update(() => {
@@ -530,6 +771,24 @@ const filterFn = (val, update) => {
     );
   });
 };
+watch(
+  () => openDeleteIva.value,
+  (value) => {
+    if (!value) {
+      retIvaData.value = null;
+    }
+  },
+  { deep: true }
+);
+watch(
+  () => openFormRetIva.value,
+  (value) => {
+    if (!value) {
+      retIvaData.value = null;
+    }
+  },
+  { deep: true }
+);
 watch(
   () => openDeleteIva.value,
   (value) => {
@@ -576,6 +835,12 @@ watch(
   paisIslr,
   debounce((value) => {
     getListIslr();
+  }, 500)
+);
+watch(
+  paisRetencionIva,
+  debounce((value) => {
+    getListRetIva();
   }, 500)
 );
 </script>
